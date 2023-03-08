9374a76d3b6f24cc453d6ef3b4aea76736a6a02d
WackyMole <wacky@wackymole.com>
2023-03-07 18:36:25 -0600

    working on img conversions

--- Utility/SpriteTools.cs
+++ Utility/SpriteTools.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
+
 
 namespace RainbowTrollArmor
 {
@@ -173,6 +175,57 @@ namespace RainbowTrollArmor
         public Sprite getSprite()
         {
             return outcome;
+        }
+
+
+        public static Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 100.0f, SpriteMeshType spriteType = SpriteMeshType.Tight)
+        {
+
+            // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
+
+            Texture2D SpriteTexture = LoadTexture(FilePath);
+            Sprite NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit, 0, spriteType);
+
+            return NewSprite;
+        }
+
+        public static Sprite ConvertTextureToSprite(Texture2D texture, float PixelsPerUnit = 100.0f, SpriteMeshType spriteType = SpriteMeshType.Tight)
+        {
+            // Converts a Texture2D to a sprite, assign this texture to a new sprite and return its reference
+
+            Sprite NewSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), PixelsPerUnit, 0, spriteType);
+
+            return NewSprite;
+        }
+
+        public static Texture2D LoadTexture(string FilePath)
+        {
+
+            // Load a PNG or JPG file from disk to a Texture2D
+            // Returns null if load fails
+            Texture2D Tex2D;
+            byte[] FileData;
+
+            if (File.Exists(FilePath))
+            {
+                FileData = File.ReadAllBytes(FilePath);
+                Tex2D = new Texture2D(2, 2);           // Create new "empty" texture
+                if (Tex2D.LoadImage(FileData))           // Load the imagedata into the texture (size is set automatically)
+                    return Tex2D;                 // If data = readable -> return texture
+            }
+            return null;                     // Return null if load failed
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
 
 
