50b3cbcf0361f151ba623e50cd835cdc8239a98e
WackyMole <wacky@wackymole.com>
2023-11-03 11:48:30 -0500

    Update api

--- API.cs
+++ API.cs
@@ -12,23 +12,31 @@ public class WackyDatabase_API
 
     private static readonly bool _IsInstalled;
     private static MethodInfo eAddBlacklistClone;
+    private static MethodInfo eGetClonedMap;
 
     public static bool IsInstalled() => _IsInstalled;
     public static void AddBlacklistClone(string value)
     {
         eAddBlacklistClone?.Invoke(null, new object[] { value });
     }
+    public static string GetClonedMap(string maybeclone)
+    {
+        string result ="";
+        if (eGetClonedMap != null) result =  (string)eGetClonedMap.Invoke(null, new object[] { maybeclone });
+        return result;
+    }
 
     static WackyDatabase_API()
     {
-            if (Type.GetType("API.WackyAPI, WackysDatabase") == null)
+        if (Type.GetType("API.WackyAPI, WackysDatabase") == null)
             {
             _IsInstalled = false;
             return;
-        }
+            }
         Type wackydatabaseAPI = Type.GetType("API.WackyAPI, WackysDatabase");
         _IsInstalled = true;
         eAddBlacklistClone = wackydatabaseAPI.GetMethod("AddBlacklistClone", BindingFlags.Public | BindingFlags.Static);
+        eGetClonedMap = wackydatabaseAPI.GetMethod("GetClonedMap", BindingFlags.Public | BindingFlags.Static);
     }
 }
 
@@ -40,6 +48,18 @@ public static class WackyAPI
         //WMRecipeCust.WLog.LogInfo("Added to blacklist "+ value);
 
         WMRecipeCust.AddBlacklistClone(value);
+    }
+
+    public static string GetClonedMap(string maybeclone)
+    {
+        if (WMRecipeCust.ClonedPrefabsMap.ContainsKey(maybeclone))
+        {
+            return WMRecipeCust.ClonedPrefabsMap[maybeclone];
+        }
+        else
+        {
+            return "";
+        }
     }
 
 }
