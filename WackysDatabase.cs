116c62625a38956ef31dcae8399d91c5ee57ea1c
WackyMole <wacky@wackymole.com>
2026-05-09 20:32:25 -0500

    2.4.80 release

--- WackysDatabase.cs
+++ WackysDatabase.cs
@@ -43,7 +43,7 @@ namespace wackydatabase
     public class WMRecipeCust : BaseUnityPlugin
     {
         internal const string ModName = "WackysDatabase";
-        internal const string ModVersion = "2.4.79";
+        internal const string ModVersion = "2.4.80";
         internal const string Author = "WackyMole";
         internal const string ModGUID = Author + "." + ModName;
         internal static string ConfigFileName = ModGUID + ".cfg";
